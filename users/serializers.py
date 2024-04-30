de544d7bbe1db438f339cf0d09f08d631aa771c9
Роман Слюсар <60191989+rosomaha209@users.noreply.github.com>
2024-05-01 00:32:06 +0200

    fix: image field

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,21 @@
 from django.conf import settings
-from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+
+from users.models import CustomUser
+
+
+class CustomImageField(serializers.ImageField):
+    def to_representation(self, value):
+        if not value:
+            return None
+        return f'{settings.SITE_URL}{value.url}'
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-    profile_pic = serializers.SerializerMethodField()  # Отримання URL зображення профілю
+    profile_pic = CustomImageField(max_length=None, use_url=True, required=False, allow_null=True, default=None)
 
     class Meta:
         model = CustomUser
@@ -23,27 +30,17 @@ class CustomUserSerializer(serializers.ModelSerializer):
             'is_staff': {'read_only': True},
         }
 
-    def get_profile_pic(self, obj):
-        if obj.profile_pic:
-            return f'{settings.SITE_URL}{obj.profile_pic.url}'
-        return None
-
     def validate(self, data):
         if data.get('password1') != data.get('password2'):
             raise serializers.ValidationError({"password2": "Passwords must match."})
         return data
 
     def create(self, validated_data):
-        # Видаляємо поля пароля з validated_data, перед створенням користувача
         password1 = validated_data.pop('password1', None)
-        password2 = validated_data.pop('password2', None)
-
-        # Перевірка, чи паролі збігаються, вже виконана у методі validate, тому можемо використовувати password1
         user = CustomUser.objects.create(**validated_data)
         if password1:
             user.set_password(password1)
         user.save()
-
         return user
 
     def update(self, instance, validated_data):
